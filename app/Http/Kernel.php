76a4dfbb22378ca6e35280a21f28e32d8ad3f42a
Jotham Hernandez <jotham_hernandez@hotmail.com>
2019-04-08 00:26:38 +0800

    #2 added middleware for completing the personal information before registering to an event

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'super_admin' => \App\Http\Middleware\Access\RedirectMembers::class,
+        'completely_verified' => \App\Http\Middleware\Access\VerifiedAndComplete::class,
     ];
 
     /**
