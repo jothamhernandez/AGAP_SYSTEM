a4bb8bb4a840d8999b2db51d639d3f616f186b12
Jotham Hernandez <jotham_hernandez@hotmail.com>
2019-05-20 12:15:29 +0800

    fixed import

---
+++ app/Http/Controllers/ImportController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Imports\AgencyImports;
+use App\Models\Department;
+use App\Models\Agency;
+
+class ImportController extends Controller
+{
+    //
+    public function import_agencies(Request $request){
+
+        $file = $request->file('agency-csv');
+
+        $collection = (new AgencyImports)->toArray($file);
+        
+        // dd($collection);
+
+        collect($collection[0])->each(function($row, $index){
+            if($department = Department::where('display_name', $row['department'])->first()){
+                if($agency = Agency::where('name',$row['agency'])->where('sector', $row['department'])->first()){
+                    $agency->department_id = $department->id;
+                    $agency->save();
+                } else {
+                    Agency::create(['sector'=>$row['department'], 'name'=>$row['agency'], 'department_id'=>$department->id, 'bsgc_status'=>$row['bsgc_status']]);
+                }
+                
+                
+            } else {
+                Department::create(['display_name'=>$row['department']]);
+            }
+        });
+
+        echo "DONE"; die();
+    }
+}
