c813b90ae332451b4fd28e2582128c05a37dd020
Jotham Hernandez <jotham_hernandez@hotmail.com>
2019-09-04 20:58:01 +0800

    updated dashboard page

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -11,7 +11,8 @@ class MembersController extends Controller
     public function dashboard(Request $request){
         // dd($request->ip());
         $sessions = \App\Models\LoggedSessions::where(['user_id'=>$request->user()->id])->orderBy('created_at','desc')->get();
+        $events = \App\Models\RegisteredUser::where(['user_id'=>$request->user()->id])->count();
         
-        return view('pages.dashboard',['sessions'=>$sessions]);
+        return view('pages.dashboard',['sessions'=>$sessions, 'events'=>$events]);
     }
 }
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -3,33 +3,51 @@
 @section('content')
 
 <div class="container">
-    <div class="card">
-        <div class="card-header">
-            <div class="card-title">Logged Sessions</div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="card-title">
+                        <h3>Logged Sessions</h3>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <table class="table table-fluid table-stripped">
+                        <thead>
+                            <tr>
+                                <th>IP Address</th>
+                                <th>Location</th>
+                                <th>Region</th>
+                                <th>City</th>
+                                <th>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($sessions as $session)
+                            <tr>
+                                <td>{{$session->ip_address}}</td>
+                                <td>{{$session->country_code}}</td>
+                                <td>{{$session->region_name}}</td>
+                                <td>{{$session->city_name}}</td>
+                                <td>{{$session->created_at}}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-        <div class="card-body">
-            <table class="table table-fluid table-stripped">
-                <thead>
-                    <tr>
-                        <th>IP Address</th>
-                        <th>Location</th>
-                        <th>Region</th>
-                        <th>City</th>
-                        <th>Date</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($sessions as $session)
-                    <tr>
-                        <td>{{$session->ip_address}}</td>
-                        <td>{{$session->country_code}}</td>
-                        <td>{{$session->region_name}}</td>
-                        <td>{{$session->city_name}}</td>
-                        <td>{{$session->created_at}}</td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+    </div>
+    <div class="row mt-4">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    <h3>Registered Event</h3>
+                </div>
+                <div class="card-body">
+                    <div class="jumbotron text-center"><h2>{{$events}}</h2></div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
