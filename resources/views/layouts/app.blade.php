29e0999c89339be9eda329309a6228faf8365e68
Jotham Hernandez <jotham_hernandez@hotmail.com>
2019-09-18 14:54:48 +0800

    added description for seo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="AGAP Portal focusing on AGAP Events, Register to be updated on AGAP Events">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
