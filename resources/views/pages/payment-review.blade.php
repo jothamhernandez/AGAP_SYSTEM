83bda5147f3d703bf0e786167742c97cab27da02
Jotham Hernandez <jotham_hernandez@hotmail.com>
2019-10-12 11:40:14 +0800

    fixed report payment review

--- resources/views/pages/payment-review.blade.php
+++ resources/views/pages/payment-review.blade.php
@@ -13,7 +13,7 @@
     @endif
     <div class="row">
         <div class="col-md-12">
-        <a href="{{route('page.reports')}}" class="btn btn-primary btn-sm">Back</a>
+            <a href="{{route('page.reports')}}" class="btn btn-primary btn-sm">Back</a>
         </div>
     </div>
     <div class="row">
@@ -21,21 +21,38 @@
             <h1>Payment Review: {{$event->title}}</h1>
         </div>
         <div class="col-md-12">
+
+            <div class="card card-default mb-5">
+                <div class="card-header agap-primary-color">
+                    <div class="card-title">Status Filter</div>
+                </div>
+                <div class="card-body">
+                    <form action="{{request()->fullUrl()}}" id="filter_query">
+                        <span style="padding:5px;"><input type="radio" class="filter_radio" name="status" value="pending" @if( @request()->status == "pending" || @request()->status == null ) checked @endif> Pending</span>
+                        <span style="padding:5px;"><input type="radio" class="filter_radio" name="status" value="rejected" @if( @request()->status == "rejected" ) checked @endif> Rejected</span>
+                        <span style="padding:5px;"><input type="radio" class="filter_radio" name="status" value="unpaid" @if( @request()->status == "unpaid" ) checked @endif> Unpaid</span>
+                        <span style="padding:5px;"><input type="radio" class="filter_radio" name="status" value="paid" @if( @request()->status == "paid" ) checked @endif> Paid</span>
+                        <span style="padding:5px;"><input type="radio" class="filter_radio" name="status" value="cancelled" @if( @request()->status == "cancelled" ) checked @endif> Cancelled</span>
+                        <span></span>
+                    </form>
+                </div>
+            </div>
+
             <table class="table table-striped table-fluid table-sm">
                 <thead>
-                    
+
                     <tr>
-                        <td>Member</td>
-                        <td>Department</td>
-                        <td>Agency</td>
-                        <td>Fee</td>
-                        <td>Status</td>
-                        <td>Action</td>
+                        <th>Member</th>
+                        <th>Department</th>
+                        <th>Agency</th>
+                        <th>Fee</th>
+                        <th>Status</th>
+                        <th>Action</th>
                     </tr>
-                    
+
                 </thead>
                 <tbody>
-                @foreach($event->registered as $registered)
+                    @foreach($registered_users as $registered)
                     <tr>
                         <td style="vertical-align: middle;">{{$registered->user->info->fullname()}}</td>
                         <td style="vertical-align: middle;">{{@$registered->user->info->agency->department->display_name}}</td>
@@ -50,7 +67,7 @@
                             @if($registered->status != "Paid")
                             <select name="" id="" class="form-control" data-status="{{$registered->id}}">
                                 @foreach($registered->getEnumValues()['status'] as $val)
-                                <option value="{{$val}}" @if($val == $registered->status) selected  @endif>{{$val}}</option>
+                                <option value="{{$val}}" @if($val==$registered->status) selected @endif>{{$val}}</option>
                                 @endforeach
                             </select>
                             @else
@@ -60,15 +77,18 @@
                         <td style="vertical-align: middle;">
                             @if($registered->status != "Paid")
                             <form action="{{route('payment.paid',['registered_id'=>$registered->id])}}" method="POST" id="registered-{{$registered->id}}">
-                            @csrf
+                                @csrf
                             </form>
                             <button class="btn btn-success btn-sm btn-block" data-toggle="confirmation" data-title="Save Status Change" data-focus="{{$registered->id}}">Save</button>
                             @endif
                         </td>
                     </tr>
-                @endforeach
+                    @endforeach
                 </tbody>
+
+
             </table>
+            {{ $registered_users->links() }}
         </div>
     </div>
 </div>
@@ -77,10 +97,10 @@
 
 @section('scripts')
 <script>
-    window.onload = function(){
-        $('[data-status]').on('change', function(e){
+    window.onload = function() {
+        $('[data-status]').on('change', function(e) {
             var option = this;
-            if($(`#changer-${$(this).data('status')}`).length < 1){
+            if ($(`#changer-${$(this).data('status')}`).length < 1) {
                 $(`#registered-${$(this).data('status')}`).append($(`<input type="hidden" id="changer-${$(this).data('status')}" name="status" value="${$(this).val()}"/>`))
             } else {
                 $(`#changer-${$(this).data('status')}`).val($(this).val());
@@ -90,10 +110,14 @@
         $('[data-toggle=confirmation]').confirmation({
             rootSelector: '[data-toggle=confirmation]',
             // other options
-            onConfirm: function(e){
+            onConfirm: function(e) {
                 var form = $(`#registered-${$(this).data('focus')}`);
                 form.submit();
             }
+        });
+
+        $('.filter_radio').on('change', function(e) {
+            $('#filter_query').submit();
         });
 
     }
